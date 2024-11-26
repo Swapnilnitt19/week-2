e45b1a1ae077e8d37e336e76348ded44cc3ba5f2
Swapnilnitt19 <swapnil.nitt1630@gmail.com>
2024-11-26 15:11:02 +0530

    Add files via upload

---
+++ 3 numbers equal or not .c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+	int a ,b,c;
+	printf(" enter a :\n");
+	scanf("%d",&a);
+	printf(" enter b :\n");
+	scanf("%d",&b);
+	printf("enter c : \n");
+	scanf("%d",&c);
+	
+	if(a==b==c){
+		printf(" all number are equal ");
+	}
+	else 
+	{ 
+	printf(" numbers are not equal ");
+	}
+	return 0;
+	}
