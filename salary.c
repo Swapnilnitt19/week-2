20514c15ba4ee2d2dcac076aaeacfb9a148975f4
Swapnilnitt19 <swapnil.nitt1630@gmail.com>
2024-11-26 15:11:15 +0530

    Add files via upload

---
+++ salary.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int basic , da, hra;
+	printf(" enter the basic salary : \n");
+	scanf("%d",&basic);
+	
+	da=basic*50/100;
+	hra=basic*10/100;
+	printf(" total salary is %d\n ",basic+da+hra);
+	return 0;
+}
